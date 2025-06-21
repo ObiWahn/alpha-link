1134d29d06d3876f377ec41b0456fe15a2b7f173
Jan Christoph Uhde <jan@uhde.io>
2025-06-21 21:39:36 +0200

    add: finish diff_state function

--- include/alink/detail/diff_state.hpp
+++ include/alink/detail/diff_state.hpp
@@ -7,6 +7,8 @@
 
 #include "../traits.hpp"
 
+#include <iostream>
+
 namespace alink::detail {
 
 template <typename Key, typename Value>
@@ -15,36 +17,196 @@ struct diff {
     std::vector<Key> remove;
 };
 
+namespace diff_detail {
 
-// forward range must be sorted
-template <typename Key, typename Value>
+template <typename K, typename V>
+inline K const& access_key(std::pair<K, V> const& x) {
+    return x.first;
+}
+
+template <typename K, typename V>
+inline V& access_value(std::pair<K, V>& x) {
+    return x.second;
+}
+
+template <typename K, typename V>
+inline V& access_value(std::pair<K, std::shared_ptr<V>>& x) {
+    return *(x.second);
+}
+
+template <typename K, typename V>
+inline V const& access_value(std::pair<K, V> const& x) {
+    return x.second;
+}
+
+template <typename K, typename V>
+inline V const& access_value(std::pair<K, std::shared_ptr<V>> const& x) {
+    return *(x.second);
+}
+
+// This is required, because moving from an lvalue ref input range is
+// forbidden otherwise we would not need to use move_key and move_value.
+template <bool debug, bool move_key, bool move_value, typename K, typename V>
+std::pair<K, std::shared_ptr<V>> create_upsert_pair(std::pair<K, V>& p) {
+    if (move_key && move_value) {
+        if constexpr (debug)
+            std::cerr << "move key&value from pair\n";
+        return {std::move(p.first), std::make_shared<V>(std::move(p.second))};
+    } else if (move_value) {
+        if constexpr (debug)
+            std::cerr << "move value from pair\n";
+        return {p.first, std::make_shared<V>(std::move(p.second))};
+    } else {
+        if constexpr (debug)
+            std::cerr << "copy from pair\n";
+        return {p.first, std::make_shared<V>(p.second)};
+    }
+}
+
+template <bool debug, bool move_key, bool move_value, typename K, typename V>
+std::pair<K, std::shared_ptr<V>> create_upsert_pair(std::pair<K, V> const& p) {
+    if constexpr (debug)
+        std::cerr << "copy from pair\n";
+    return {p.first, std::make_shared<V>(p.second)};
+}
+
+template <bool debug, bool move_key, bool move_value, typename K, typename V>
+std::pair<K, std::shared_ptr<V>> create_upsert_pair(std::pair<K, std::shared_ptr<V>>& p) {
+    if constexpr (debug)
+        std::cerr << "move pair\n";
+    return std::move(p);
+}
+
+template <bool debug, bool move_key, bool move_value, typename K, typename V>
+std::pair<K, std::shared_ptr<V>> create_upsert_pair(std::pair<K, std::shared_ptr<V>> const& p) {
+    if constexpr (debug)
+        std::cerr << "copy pair\n";
+    return p;
+}
+
+} // namespace diff_detail
+
+//! \brief creates a diff between current_state and new_state, optionally updating current state
+/**
+ *  \param current_state: reference to current state
+ *  \param forward_range: rage representing the new_state
+ *  \param update_state (template): controls if the current_state should be updated
+ *
+ *  \return diff of both states
+ *
+ *  \warning THE `new_state` RANGE MUST BE SORTED!!!!! (Use `#define ALINK_ALLOW_HEAVY_RUNTIME_CHECKS true` to check)
+ *  \note The range MUST contain either `std::pair<Key,Value>` or `std::pair<Key, std::shared_ptr<Value>>`
+ *        (so this function supports both std::vector and std::map as ranges)
+ *  \note in order to allow moving values from `new_state` it has to be moved into the fuction
+ *
+ */
+template <typename Key, typename Value, std::ranges::forward_range T, bool update_state = true>
     requires requires(Value a, Value b) {
         { traits<Value>::compare(a, b) } -> std::convertible_to<bool>;
     }
-auto diff_states(std::map<Key, std::shared_ptr<Value>>& current_state, std::ranges::forward_range auto new_state)
-    -> diff<Key, Value> {
+auto diff_state(std::map<Key, std::shared_ptr<Value>>& current_state, T&& new_state) -> diff<Key, Value> {
+
+    constexpr bool debug_template = false;
+    constexpr bool debug = false;
+
+    constexpr bool is_rvalue_range = std::is_rvalue_reference_v<decltype(new_state)>;
+    // could be done with conditional and static_cast<T&&>
+    using range_type = std::remove_reference_t<decltype(new_state)>;
+    constexpr bool is_const_range = std::is_const_v<range_type>;
+    using range_value_type = std::ranges::range_value_t<range_type>;
+    constexpr bool range_has_shared_ptr_values =
+        std::is_same_v<std::shared_ptr<Value>, typename range_value_type::second_type>;
+
+    // the constness of the value is removed here
+    if constexpr (range_has_shared_ptr_values)
+        static_assert(std::is_same_v<range_value_type, std::pair<Key, std::shared_ptr<Value>>> ||
+                          std::is_same_v<range_value_type, std::pair<Key const, std::shared_ptr<Value>>>,
+                      "The new_state does not match the provided current_state. "
+                      "The iterator value should be of the form `std::pair<Key, Value>` or "
+                      "`std::pair<Key, std::shared_ptr<Value>>`. Additional const qualifiers are allowed");
+    else
+        static_assert(std::is_same_v<range_value_type, std::pair<Key, Value>> ||
+                          std::is_same_v<range_value_type, std::pair<Key const, Value>>,
+                      "The new_state does not match the provided current_state. "
+                      "The iterator value should be of the form `std::pair<Key, Value>` or "
+                      "`std::pair<Key, std::shared_ptr<Value>>`. Additional const qualifiers are allowed");
+
+    using iter_type = std::ranges::iterator_t<T>;      // here we get the iterator which might be const or not
+    using ref_type = std::iter_reference_t<iter_type>; // e.g std::pair<int, int> const&
+    // the (( )) is important so we get the expression type and not how the member is declared
+    using key_ref_type = decltype((std::declval<ref_type>().first));
+    using value_ref_type = decltype((std::declval<ref_type>().second));
+    constexpr bool const_key = std::is_const_v<std::remove_reference_t<key_ref_type>>;
+    constexpr bool const_value = std::is_const_v<std::remove_reference_t<value_ref_type>>;
+
+    constexpr bool do_move_key = is_rvalue_range && !const_key;
+    constexpr bool do_move_value = is_rvalue_range && !const_value;
+
+    if constexpr (debug_template) {
+        std::cerr << "is_rvalue_range: " << is_rvalue_range << "\nnis_const_range: " << is_const_range
+                  << "\nrange_has_shared_ptr_values: " << range_has_shared_ptr_values
+                  << "\nconst_value: " << const_value << "\nconst_key: " << const_key
+                  << "\ndo_move_key: " << do_move_key << "\ndo_move_value: " << do_move_value << "\n\n";
+    }
+
     auto current_it = current_state.begin();
     auto new_it = new_state.begin();
     auto result = diff<Key, Value>{};
 
-    using range_type = std::remove_cvref_t<decltype(new_state)>;
-    using range_value_type = std::ranges::range_value_t<range_type>;
-    static_assert(std::is_same_v<typename std::ranges::range_value_t<range_type>, std::pair<Key, Value>>,
-                  "the rage does not match Key and Value provided for the map");
-
     while (current_it != current_state.end() && new_it != new_state.end()) {
-        if (current_it->first < new_it->first) {
-            // old key not in new_map -> erase single and move on
-            result.remove.push_back(current_it->first);
-            ++current_it;
-        } else if (current_it->first > new_it->first) {
-            // new key not in old_map -> insert
-            result.upsert.push_back(std::pair{new_it->first, std::make_shared<Value>(new_it->second)});
+        Key const& current_key = diff_detail::access_key(*current_it);
+        Value const& current_value = diff_detail::access_value(*current_it);
+        Key const& new_key = diff_detail::access_key(*new_it);
+        Value const& new_value = diff_detail::access_value(*new_it);
+
+        if constexpr (debug) {
+            std::cerr << "\n--------\ncurrent " << current_key << " - " << current_value << std::endl;
+            std::cerr << "new " << new_key << " - " << new_value << std::endl << std::endl;
+        }
+
+        if (std::less<>{}(current_key, new_key) ) {
+            // new key not in current -> upsert
+            std::cerr << "case A\n";
+            result.remove.push_back(current_key);
+
+            if constexpr (debug)
+                std::cerr << "erase " << current_key << std::endl;
+
+            if constexpr (update_state)
+                current_state.erase(current_it++);
+            else
+                ++current_it;
+
+        } else if (std::less<>{}(new_key, current_key)) {
+            // new key not in current -> upsert
+            std::cerr << "case B\n";
+            result.upsert.push_back(diff_detail::create_upsert_pair<debug, do_move_key, do_move_value>(*new_it));
+
+            if constexpr (update_state) {
+                current_it = current_state.insert(current_it, result.upsert.back());
+                ++current_it;
+            }
+
+            if constexpr (debug)
+                std::cerr << "insert " << new_key << " - " << new_value << std::endl;
+
             ++new_it;
         } else {
-            // keys equal -> update value
-            if (!traits<Value>::compare(*(current_it->second), new_it->second))
-                result.upsert.push_back(std::pair{new_it->first, std::make_shared<Value>(new_it->second)});
+            // keys equal -> upsert
+            std::cerr << "case C\n";
+            if (!traits<Value>::compare(current_value, new_value)) {
+                result.upsert.push_back(diff_detail::create_upsert_pair<debug, do_move_key, do_move_value>(*new_it));
+
+                if constexpr (update_state)
+                    current_it->second = result.upsert.back().second;
+
+                if constexpr (debug) {
+                    std::cerr << "update " << new_key << " - " << new_value << std::endl;
+                    std::cerr << "update " << result.upsert.back().first << " - " << *(result.upsert.back().second)
+                              << std::endl;
+                }
+            }
+
             ++current_it;
             ++new_it;
         }
@@ -52,19 +214,46 @@ auto diff_states(std::map<Key, std::shared_ptr<Value>>& current_state, std::rang
 
     // Erase leftover range from current_state
     while (current_it != current_state.end()) {
+        std::cerr << "case D\n";
         result.remove.push_back(current_it->first);
-        ++current_it;
-    }
-    // current_map.erase(old_it, current_map.end());
 
-    // // Insert remaining new keys
-    // current_map.insert(new_it, new_state.end());
+        if constexpr (debug) {
+            std::cerr << "erase " << current_it->first << std::endl;
+        }
+
+        if constexpr (update_state)
+            current_state.erase(current_it++);
+        else
+            ++current_it;
+    }
+
+    // upsert remaining new pairs
     while (new_it != new_state.end()) {
-        result.upsert.push_back(std::pair{new_it->first, std::make_shared<Value>(new_it->second)});
+        std::cerr << "case E\n";
+        result.upsert.push_back(diff_detail::create_upsert_pair<debug, do_move_key, do_move_value>(*new_it));
+
+        if constexpr (update_state)
+            current_state.insert(result.upsert.back());
+
+        if constexpr (debug) {
+            std::cerr << "insert " << new_it->first << " - " << diff_detail::access_value(*new_it) << std::endl;
+        }
+
         ++new_it;
     }
 
     return result;
+}
+
+template <typename Key,
+          typename Value,
+          std::ranges::forward_range T,
+          bool update_state = true>
+    requires requires(Value a, Value b) {
+        { traits<Value>::compare(a, b) } -> std::convertible_to<bool>;
+    }
+auto diff_and_update_state(std::map<Key, std::shared_ptr<Value>>& current_state, T&& new_state) -> diff<Key, Value> {
+    return diff_state<Key, Value, T, true>(current_state, std::forward<T>(new_state));
 }
 
 } // namespace alink::detail
--- tests/diff_state.cpp
+++ tests/diff_state.cpp
@@ -13,11 +13,41 @@ struct traits<int> : std::true_type {
 
 } // namespace alink
 
-using key_vec = std::vector<int>;
-using pair_vec = std::vector<std::pair<int, std::shared_ptr<int>>>;
+using vec = std::vector<int>;
+using vec_pair = std::vector<std::pair<int, int>>;
+using vec_pair_shared = std::vector<std::pair<int, std::shared_ptr<int>>>;
+using state_map = std::map<int, std::shared_ptr<int>>;
 
 namespace {
-bool equal_lists(pair_vec const& a, pair_vec const& b) {
+
+template <typename T>
+    requires std::same_as<T, vec_pair_shared> || std::same_as<T, state_map>
+bool compare_sorted_equal(T const& a, T const& b) {
+    using namespace alink::detail::diff_detail;
+
+    if (a.size() != b.size())
+        return false;
+
+    auto it_a = a.begin();
+    auto it_b = b.begin();
+
+    while (it_a != a.end()) {
+        if (it_a->first != it_b->first)
+            return false;
+        if (!it_a->second || !it_b->second) {
+            if (it_a->second != it_b->second)
+                return false;
+        } else if (access_value(*it_a) != access_value(*it_b)) {
+            return false;
+        }
+        ++it_a;
+        ++it_b;
+    }
+
+    return true;
+}
+
+bool equal_states(state_map const& a, state_map const& b) {
     if (a.size() != b.size())
         return false;
 
@@ -40,45 +70,152 @@ bool equal_lists(pair_vec const& a, pair_vec const& b) {
 
     return true;
 }
+
+auto ms(auto k, auto v) {
+    return std::pair{k, std::make_shared<std::remove_cvref_t<decltype(v)>>(v)};
+}
+
+template <typename T>
+auto id_map(std::vector<T> vec) {
+    std::map<T, T> rv;
+    for (auto item : vec) rv[item] = item;
+    return rv;
+}
+
+template <typename T>
+auto id_map_shared(std::vector<T> vec) {
+    std::map<T, std::shared_ptr<T>> rv;
+    for (auto item : vec) rv[item] = std::make_shared<T>(item);
+    return rv;
+}
+
 } // namespace
 
 
-TEST(diff_states, empty) {
-    std::map<int, std::shared_ptr<int>> state;
-    std::vector<std::pair<int, int>> next;
+TEST(diff_state, empty) {
+    state_map state;
+    vec_pair next_state;
 
-    auto diff = alink::detail::diff_states(state, next);
+    auto diff = alink::detail::diff_state(state, next_state);
 
-    EXPECT_EQ(diff.remove, key_vec{});
-    EXPECT_EQ(diff.upsert, pair_vec{});
+    EXPECT_EQ(diff.remove, vec{});
+    EXPECT_EQ(diff.upsert, vec_pair_shared{});
 }
 
-TEST(diff_states, simple) {
-    std::map<int, std::shared_ptr<int>> state = {
-        {1, std::make_shared<int>(1)},
-        {2, std::make_shared<int>(2)},
-        {3, std::make_shared<int>(3)},
-        {4, std::make_shared<int>(4)},
-        {5, std::make_shared<int>(5)},
-        {6, std::make_shared<int>(6)},
-        {7, std::make_shared<int>(7)},
-    };
-    std::vector<std::pair<int, int>> next = {{2, 4}, {3, 3}, {4, 8}, {5, 5}, {8, 16}};
+TEST(diff_state, vector_input) {
+    auto state = id_map_shared(vec{1, 2, 3, 4, 5, 6, 7});
+    vec_pair next_state = {{2, 4}, {3, 3}, {4, 8}, {5, 5}, {8, 16}};
 
-    auto diff = alink::detail::diff_states(state, next);
-
+    auto diff = alink::detail::diff_and_update_state(state, std::move(next_state));
     {
         auto& actual = diff.remove;
-        auto expected = key_vec{1, 6, 7};
+        auto expected = vec{1, 6, 7};
         EXPECT_EQ(actual, expected);
     }
     {
         auto& actual = diff.upsert;
-        auto expected = pair_vec{
-            {2, std::make_shared<int>(4)},
-            {4, std::make_shared<int>(8)},
-            {8, std::make_shared<int>(16)},
+        auto expected = vec_pair_shared{
+            ms(2, 4),
+            ms(4, 8),
+            ms(8, 16),
         };
-        EXPECT_TRUE(equal_lists(actual, expected));
+        auto result = compare_sorted_equal(actual, expected);
+        if (!result)
+            EXPECT_EQ(actual, expected);
+        EXPECT_TRUE(result);
     }
+}
+
+TEST(diff_state, const_vector_input) {
+    auto state = id_map_shared(vec{ 3, 4, 5, 6, 7});
+    vec_pair const next_state = {
+        {2, 4},
+        {3, 3},
+        {4, 8},
+    };
+
+    auto diff = alink::detail::diff_and_update_state(state, next_state);
+
+    {
+        auto& actual = diff.remove;
+        auto expected = vec{5, 6, 7};
+        EXPECT_EQ(actual, expected);
+    }
+    {
+        auto& actual = diff.upsert;
+        auto expected = vec_pair_shared{
+            ms(2, 4),
+            ms(4, 8),
+        };
+        auto result = compare_sorted_equal(actual, expected);
+        if (!result)
+            EXPECT_EQ(actual, expected);
+        EXPECT_TRUE(result);
+    }
+    {
+        auto expected = state_map{
+            ms(2, 4),
+            ms(3, 3),
+            ms(4, 8),
+        };
+        auto result = compare_sorted_equal(state, expected);
+        if (!result)
+            EXPECT_EQ(state, expected);
+        EXPECT_TRUE(result);
+    }
+}
+
+TEST(diff_state, rvalue_state_map_input) {
+    auto state = id_map_shared(vec{1, 2, 3, 4, 5, 6, 7});
+    state_map next_state = {ms(2, 4), ms(3, 3), ms(4, 8), ms(5, 5), ms(8, 16)};
+
+    auto diff = alink::detail::diff_state(state, std::move(next_state));
+
+    {
+        auto& actual = diff.remove;
+        auto expected = vec{1, 6, 7};
+        EXPECT_EQ(actual, expected);
+    }
+    {
+        auto& actual = diff.upsert;
+        auto expected = vec_pair_shared{
+            ms(2, 4),
+            ms(4, 8),
+            ms(8, 16),
+        };
+        auto result = compare_sorted_equal(actual, expected);
+        if (!result)
+            EXPECT_EQ(actual, expected);
+        EXPECT_TRUE(result);
+    }
+}
+
+TEST(diff_state, const_state_map_input) {
+    auto state = id_map_shared(vec{1, 2, 3, 4, 5, 6, 7});
+    state_map const next_state = {ms(2, 4), ms(3, 3), ms(4, 8), ms(5, 5), ms(8, 16)};
+
+    auto diff = alink::detail::diff_state(state, next_state);
+
+    {
+        auto& actual = diff.remove;
+        auto expected = vec{1, 6, 7};
+        EXPECT_EQ(actual, expected);
+    }
+    {
+        auto& actual = diff.upsert;
+        auto expected = vec_pair_shared{
+            ms(2, 4),
+            ms(4, 8),
+            ms(8, 16),
+        };
+        auto result = compare_sorted_equal(actual, expected);
+        if (!result)
+            EXPECT_EQ(actual, expected);
+        EXPECT_TRUE(result);
+    }
+
+    auto result = compare_sorted_equal(state, next_state);
+    if (!result)
+        EXPECT_EQ(state, next_state);
+    EXPECT_TRUE(result);
 }
